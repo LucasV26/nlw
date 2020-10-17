877911dfc8cd34b4c2f478d6e1c5a1c3e007f5af
LucasV26 <lucas2606.vinicius@gmail.com>
2020-10-17 02:02:00 -0300

    Quinto dia: Finalizando a aplicação mobile com conexões com o back-end

--- aulas/mobile/src/pages/OrphanagesMap.tsx
+++ aulas/mobile/src/pages/OrphanagesMap.tsx
@@ -1,56 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import api from '../services/api';
 
 import mapMarker from '../images/map-marker.png';
 
+// Criando uma interfaca para que o componente saiba que tipo de dado está sendo retornado do servidor (quais os parâmetros do objeto)
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
 export default function OrphanagesMap() {
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+    // Use effect para executar a chamada do servidor apenas uma vez
+    useFocusEffect(() => {
+      api.get('orphanages').then(response => {
+        setOrphanages(response.data); // Coletando o response da nossa rota do servidor back-end
+      });
+    }); // O array do segundo parâmetro de useEffect() indica quando chamar essa função denovo
+
     const navigation = useNavigation();
     
-    function handleNavigateToOrphanageDetails() {
+    function handleNavigateToOrphanageDetails(id: number) {
         // Navegando a partir do 'name' configurado no arquivo routes.tsx
-        navigation.navigate('OrphanageDetails');
+        navigation.navigate('OrphanageDetails', { id }); // O objeto no segundo parâmetro são dados adicionais que eu quero enviar para a rota
     }
+
+    function handleNavigateToCreateOrphanage() {
+      // Navegando a partir do 'name' configurado no arquivo routes.tsx
+      navigation.navigate('SelectMapPosition');
+  }
 
     return (
         <View style={styles.container}>
             <MapView provider={PROVIDER_GOOGLE} style={styles.map} initialRegion={{
-                latitude: -23.4315703,
-                longitude: -46.5451525,
+                latitude: -23.439730091461957,
+                longitude: -46.566184759140015,
                 latitudeDelta: 0.008,
                 longitudeDelta: 0.008,
             }} >
 
-                <Marker icon={mapMarker} calloutAnchor={{
-                    x: 2.4,
-                    y: 0.8,
-                }} coordinate={{
-                    latitude: -23.4315703,
-                    longitude: -46.5451525,
-                }} >
-                    {/* O callout chama a função de navegação ao ser clicado */}
-                    <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
-
-                        <View style={styles.calloutContainer}>
-
-                            <Text style={styles.calloutText}> Lar das meninas </Text>
-
-                        </View>
-
-                    </Callout>
-
-                </Marker>
+                {orphanages.map(orphanage => {
+                  return (
+                    <Marker key={orphanage.id} icon={mapMarker} calloutAnchor={{
+                      x: 2.4,
+                      y: 0.8,
+                    }} coordinate={{
+                        latitude: orphanage.latitude,
+                        longitude: orphanage.longitude,
+                    }} >
+                      {/* O callout chama a função de navegação ao ser clicado */}
+                      <Callout tooltip onPress={() => handleNavigateToOrphanageDetails(orphanage.id)}>
+  
+                          <View style={styles.calloutContainer}>
+  
+                              <Text style={styles.calloutText}> {orphanage.name} </Text>
+  
+                          </View>
+  
+                      </Callout>
+  
+                  </Marker>
+                  );
+                })}
 
             </MapView>
 
             <View style={styles.footer}>
-                <Text style={styles.footerText}> N orfanatos encontrados... </Text>
-                <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
+                <Text style={styles.footerText}> {orphanages.length} orfanatos encontrados... </Text>
+                <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
                     <Feather name='plus' size={20} color='#FFF' />
-                </TouchableOpacity>
+                </RectButton>
             </View>
         </View>
     ); // Estas tags são do React Native, não utilizamos HTML
--- aulas/mobile/src/routes.tsx
+++ aulas/mobile/src/routes.tsx
@@ -5,6 +5,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
 
 const { Navigator, Screen } = createStackNavigator();
 // Navigator => Todas as telas abertas
@@ -15,10 +18,23 @@ export default function Routes(){
         <NavigationContainer>
             {/* Configurando para que não seja mostrado um Header com nome da rota */}
             {/* Em todas as páginas */}
-            <Navigator screenOptions={{headerShown: false}}>
+            <Navigator screenOptions={{headerShown: false, cardStyle: {backgroundColor: '#f2f3f5'}}}>
 
                 <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanageDetails" component={OrphanageDetails} />
+                <Screen name="OrphanageDetails" component={OrphanageDetails} options={{
+                    headerShown: true,
+                    header: () => <Header showCancel={false} title="Orfanato" />
+                }} />
+                
+                {/* Fluxo de criação de novo orfanato: */}
+                <Screen name="SelectMapPosition" component={SelectMapPosition} options={{
+                    headerShown: true,
+                    header: () => <Header title="Selecione no mapa" />
+                }} />
+                <Screen name="OrphanageData" component={OrphanageData} options={{
+                    headerShown: true,
+                    header: () => <Header title="Insira os dados" />
+                }} />
 
             </Navigator>
 
